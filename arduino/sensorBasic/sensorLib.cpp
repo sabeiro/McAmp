c6dd291e9329cfd58a3284dd913eb4076ea0dabf
sabeiro <sabeiro@virgilio.it>
2019-04-14 20:38:40 +0200

    tone lib

--- arduino/sensorBasic/sensorLib.cpp
+++ arduino/sensorBasic/sensorLib.cpp
@@ -1,4 +1,5 @@
 #include "sensorLib.h"
+#include "pitches.h"
 
 float tempBase = 0.;
 float lightBase = 0.;
@@ -8,9 +9,20 @@ int startTime = 0;
 int currTime = 0;
 int i = 0;
 int j = 0; 
+int note = 0;
+bool panic = 1;
+
+long duration=0, cm=0;
 
 int mean = 0;
 float delta = 1./(float)N_AVERAGE;
+
+void panicButton(){
+  int state = digitalRead(PIN_BUTT);
+  if(state==HIGH){
+    panic = !panic;
+  }
+}
 
 float analogAverage(int pinN){
   mean = 0;
@@ -47,7 +59,7 @@ void levelLed(float val, float min_val){
 
 void setBase(){
   int state = digitalRead(PIN_BUTT);
-  if(state){
+  if(state==LOW){
     tempBase = analogRead(PIN_TEMP);
     lightBase = analogRead(PIN_LIGHT);
   } 
@@ -60,5 +72,31 @@ void printState(float temperature, float poti, float light){
   Serial.print(poti);
   Serial.print("; light: ");
   Serial.print(light);
+  Serial.print("; panic:");
+  Serial.print(panic);
+  Serial.print("; distance: ");
+  Serial.print(cm);
   Serial.println(""); 
+}
+
+void playTone(float val){
+  if(!panic){
+    note = map((int)val,0,1024,0,88);
+    tone(PIN_BUZZ,sNotePitches[note]);
+  }
+  else{
+    noTone(PIN_BUZZ);
+  }
+}
+
+void measureDistance(){
+  digitalWrite(PIN_TRIG, LOW);
+  delayMicroseconds(5);
+  digitalWrite(PIN_TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PIN_TRIG, LOW);
+  pinMode(PIN_ECHO, INPUT);
+  duration = pulseIn(PIN_ECHO, HIGH);
+  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  delay(5);
 }
