81f55ceede50edb580fd2e69bb78dda936837947
sabeiro <sabeiro@virgilio.it>
2019-04-14 20:39:00 +0200

    panic button

--- arduino/sensorBasic/sensorLib.h
+++ arduino/sensorBasic/sensorLib.h
@@ -12,13 +12,19 @@
 #define PIN_POTI A1
 #define PIN_LIGHT A2
 
+#define PIN_BUZZ 3
 #define PIN_BUTT 2
+#define PIN_TRIG 6
+#define PIN_ECHO 7
 
 extern bool updateTime();
 extern void levelLed(float val, float min_val);
 extern void printState(float temperature, float poti, float light);
 extern void setBase();
 extern float analogAverage(int pinN);
+extern void playTone(float val);
+extern void panicButton();
+extern void measureDistance();
 
 extern float tempBase;
 extern float lightBase;
@@ -28,5 +34,6 @@ extern int startTime;
 extern int currTime;
 extern int i;
 extern int j; 
+extern bool panic;
 
 #endif
